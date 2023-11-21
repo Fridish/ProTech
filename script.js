3118a3624cb4345ec675a5c398ad348cd9d4ff7a
OsborneLinus <linusholm1997@gmail.com>
2023-11-21 13:32:05 +0100

    Added breakpoints and buttons to carousel.

--- script.js
+++ script.js
@@ -17,24 +17,17 @@ for (i = 0; i < collapseButton.length; i++) {
 const carousel = new Glide(".glide", {
   type: "carousel",
   autoplay: 4000,
-  perView: 1,
+  breakpoints: {
+    1440: {
+      perView: 3,
+    },
+    1024: {
+      perView: 2,
+    },
+    768: {
+      perView: 1,
+    },
+  },
 });
 
 carousel.mount();
-/* const carouselButtons = document.querySelector("[data-carousel-button]");
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    const offset = button.dataset.carouselButton === "next" ? 1 : -1;
-    const slieds = button
-      .closest("[data-carousel]")
-      .querySelector("[data-slides]");
-
-    const activeSlide = slides.querySelector("[data-active]");
-    let newIndex = [...slides.children].indexOf(activeSlide) + offset;
-    if (newIndex < 0) newIndex = slides.children.length - 1;
-    if (newIndex >= slides.children.length) newIndex = 0;
-
-    slides.children[newIndex].dataset.active = true;
-    delete activeSlide.dataset.active;
-  });
-}); */
