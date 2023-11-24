4173c2dd37133a0d8b70e6736918124d90cce5d1
Frida Fernhede <fernhedefrida@gmail.com>
2023-11-24 15:13:01 +0100

    Margins and - + buttons

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ function toggleVisibility() {
     buttonContent.style.maxHeight = null;
   }
 }
-//Ser till att knappen inte funkar, utan allt content syns, när fönstret är större än 768px
+//Ser till att footerknappen inte funkar, utan allt content syns, när fönstret är större än 768px
 function updateButtonListeners() {
   for (let i = 0; i < collapseButton.length; i++) {
     let buttonContent = collapseButton[i].nextElementSibling;
@@ -70,8 +70,6 @@ window.addEventListener("resize", updateButtonListeners);
 //info section swiper
 const swiper = new Swiper("#swiper1", {
   loop: true,
-
-  // If we need pagination
   pagination: {
     el: "#swiper1 .swiper-pagination",
   },
